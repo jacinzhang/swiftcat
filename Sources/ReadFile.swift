4fb9c63b409c6b8d6ec5a8ef76ef50ca033e1849
jacinzhang <270611346@qq.com>
2024-01-20 23:06:09 +0800

    update project

--- Sources/ReadFile.swift
+++ Sources/ReadFile.swift
@@ -7,6 +7,11 @@ import ArgumentParser
 
 @main
 struct ReadFile: ParsableCommand {
+    static var configuration: CommandConfiguration {
+        let conf = CommandConfiguration(commandName: "swiftcat")
+        return conf
+    }
+    
     @Argument(help: "文件路径")
     var filePath: String
     
@@ -14,28 +19,5 @@ struct ReadFile: ParsableCommand {
     func run() throws {
         let fileContents = try String(contentsOfFile: filePath, encoding: .utf8)
         print(fileContents)
-    }
-}
-
-struct Repeat: ParsableCommand {
-    @Flag(help: "Include a counter with each repetition.")
-    var includeCounter = false
-
-    @Option(name: .shortAndLong, help: "The number of times to repeat 'phrase'.")
-    var count: Int? = nil
-
-    @Argument(help: "The phrase to repeat.")
-    var phrase: String
-
-    mutating func run() throws {
-        let repeatCount = count ?? 2
-
-        for i in 1...repeatCount {
-            if includeCounter {
-                print("\(i): \(phrase)")
-            } else {
-                print(phrase)
-            }
-        }
     }
 }
